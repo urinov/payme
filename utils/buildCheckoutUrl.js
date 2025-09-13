3ac0c6562ffad1bc01277d6d88ca6c2a885b3971
urinov <30720428+urinov@users.noreply.github.com>
2025-09-13 12:36:18 +0500

    Update buildCheckoutUrl.js

--- utils/buildCheckoutUrl.js
+++ utils/buildCheckoutUrl.js
@@ -1,1 +1,24 @@
+function toBase64(str){ return Buffer.from(str,'utf8').toString('base64'); }
+
+function buildCheckoutUrl({
+  checkoutOrigin='https://checkout.paycom.uz',
+  merchantId, orderId, amountInTiyin,
+  lang='uz', callbackUrl, callbackTimeoutMs=15000,
+  currencyIso='UZS', description, detailBase64
+}){
+  const p=[];
+  p.push(`m=${merchantId}`);
+  p.push(`ac.order_id=${orderId}`);
+  p.push(`a=${amountInTiyin}`);
+  if(lang) p.push(`l=${lang}`);
+  if(callbackUrl) p.push(`c=${encodeURIComponent(callbackUrl)}`);
+  if(callbackTimeoutMs) p.push(`ct=${callbackTimeoutMs}`);
+  if(currencyIso) p.push(`cr=${currencyIso}`);
+  if(description) p.push(`description[${lang}]=${encodeURIComponent(description)}`);
+  if(detailBase64) p.push(`detail=${detailBase64}`);
+  const b64 = toBase64(p.join(';'));
+  return `${checkoutOrigin}/base64(${b64})`;
+}
+
+export { buildCheckoutUrl };
 
